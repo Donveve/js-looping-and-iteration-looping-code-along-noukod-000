c2579ba8cd5f6a60685a22c94a04ab4674b5ae5d
Veniel Dorestave <veniel@noukod.com>
2019-02-18 18:49:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+const employeeNames = ['teddy bear', 'drone', 'doll'];
+ 
+function printBadges(employeeNames) {
+    for (let i = 0; i < employeeNames.length; i++) {
+        console.log(`Welcome ${employeeNames[i]}! you are employee `);
+    }
+ 
+    return printBadges ;
+}
+ 
+ printBadges(employeeNames) ;
